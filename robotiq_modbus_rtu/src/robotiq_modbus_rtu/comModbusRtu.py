c228e2ee6d8154d958afd2322074ddbf28f6c0f1
Marco Morales <marcomorales2022@u.northwestern.edu>
2022-08-30 13:38:12 -0500

    Fixed issues with connections

--- robotiq_modbus_rtu/src/robotiq_modbus_rtu/comModbusRtu.py
+++ robotiq_modbus_rtu/src/robotiq_modbus_rtu/comModbusRtu.py
@@ -34,17 +34,15 @@
 # Revision $Id$
 #
 # Modifed from the orginal comModbusTcp by Kelsey Hawkins @ Georgia Tech
-
-
-
 """@package docstring
 Module comModbusRtu: defines a class which communicates with Robotiq Grippers using the Modbus RTU protocol. 
-
 The module depends on pymodbus (http://code.google.com/p/pymodbus/) for the Modbus RTU client.
 """
 
 from pymodbus.client.sync import ModbusSerialClient
+from pymodbus.register_read_message import ReadHoldingRegistersResponse
 from math import ceil
+import time
 
 class communication:	
 
@@ -58,42 +56,46 @@ class communication:
           print "Unable to connect to %s" % device
           return False
       return True
-
    def disconnectFromDevice(self):
       """Close connection"""
       self.client.close()
-
    def sendCommand(self, data):   
       """Send a command to the Gripper - the method takes a list of uint8 as an argument. The meaning of each variable depends on the Gripper model (see support.robotiq.com for more details)"""
       #make sure data has an even number of elements   
       if(len(data) % 2 == 1):
          data.append(0)
-
       #Initiate message as an empty list
       message = []
-
       #Fill message by combining two bytes in one register
       for i in range(0, len(data)/2):
          message.append((data[2*i] << 8) + data[2*i+1])
-
       #To do!: Implement try/except 
       self.client.write_registers(0x03E8, message, unit=0x0009)
-
    def getStatus(self, numBytes):
       """Sends a request to read, wait for the response and returns the Gripper status. The method gets the number of bytes to read as an argument"""
       numRegs = int(ceil(numBytes/2.0))
-
       #To do!: Implement try/except 
       #Get status from the device
       response = self.client.read_holding_registers(0x07D0, numRegs, unit=0x0009)
+      retries = 50
+      # To get around spuratic - object has no attribute 'registers'
+      while not isinstance(response, ReadHoldingRegistersResponse):
+          response = self.client.read_holding_registers(0x07D0, numRegs, unit=0x0009)
+          retries -= 1
+          time.sleep(0.01)
+          #if retries <= 0: raise TypeError("Failed to get status after 50 tries") 
+              #except TypeError as e:
+              #print(e)
+
 
       #Instantiate output as an empty list
       output = []
 
       #Fill the output with the bytes in the appropriate order
       for i in range(0, numRegs):
+
          output.append((response.getRegister(i) & 0xFF00) >> 8)
          output.append( response.getRegister(i) & 0x00FF)
-      
+
       #Output the result
       return output
